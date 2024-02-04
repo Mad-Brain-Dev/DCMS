a93c08a1f4d83070c8da465be2a9be6213e8a307
AP-Porag <smauhp@gmail.com>
2024-02-04 19:09:06 +0600

    merged with rezaul

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/', function () {
 });
 Route::get('/execute-command', function () {
 //    return redirect()->route('login');
-    Artisan::call('storage:link');
+//    Artisan::call('storage:link');
     Artisan::call('migrate:fresh --seed');
     dd('All commands executed successfully');
 });
