fa49233d6a20d0420ad264032e0f74fa1a7c1fdf
razaulkarimdighi <razaulk127@gmail.com>
2024-05-19 13:43:55 +0600

    updated

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -25,7 +25,7 @@ class ClientRequest extends FormRequest
     {
         $rules =  [
             'name' => ['required'],
-            'abbr' => ['required|max:2'],
+            'abbr' => ['required|max:3'],
             'client_id' => ['nullable'],
             'nric' => ['nullable'],
             'company_name' => ['nullable'],
--- resources/views/emails/clientmail.blade.php
+++ resources/views/emails/clientmail.blade.php
@@ -1,10 +1,59 @@
-<x-mail::message>
-Your client account is ready!!
+<!doctype html>
+<html lang="en">
 
-<x-mail::button :url="'/login'">
-Button Text
-</x-mail::button>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-Thanks,<br>
-{{ config('app.name') }}
-</x-mail::message>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>DCMS</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        Dear [Client Name or Company Name],<br>
+
+                        It is our pleasure to inform you we have registered your case details in our system, pending
+                        action from our executives and field operatives. <br><br>
+
+                        We hope to begin our good work on your case within the next 2 weeks. Should you require another
+                        opportunity to discuss more information or details on your case/cases, or if you may have any
+                        concerns to highlight to us, please feel free to reply to us via this email or call us via
+                        WhatsApp @ +65 8505 5484. For updates on your case, you may access it with your registered email
+                        as follows;<br><br>
+
+                        Login Page: https://www.securre.net/dcmslogin <br>
+                        Login ID: [Email ID] <br>
+                        Password: [password.clientname.date] <br><br>
+
+                        It is an honour to be working on your case/s, and we appreciate your faith in our good service.
+                        We look forward to impressing you with our expertise, meticulous attention to detail and
+                        efficient recovery process. <br><br>
+
+                        In the meantime, please feel free to check out our website for more information and here's a
+                        small introduction on how we were listed on Lianhe Wanbao and Straits Times for our good work!
+                        (media link)
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
