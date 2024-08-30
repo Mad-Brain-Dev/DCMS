b477210d0a4b4a15a37377e01f8367658a4a793f
razaulkarimdighi <razaulk127@gmail.com>
2024-08-30 15:38:28 +0600

    date updated

--- resources/views/emails/clientmail.blade.php
+++ resources/views/emails/clientmail.blade.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-        .content{
+        .content {
             width: 600px;
             margin: 0 auto;
 
         }
-        .card{
+
+        .card {
             padding: 20px;
         }
     </style>
 </head>
+
 <body>
     <div class="content">
         <div class="card">
-            Dear {{$client->name}},<br>
+            Dear {{ $client->name }},<br>
 
             It is our pleasure to inform you we have registered your case details in our system, pending
             action from our executives and field operatives. <br><br>
@@ -30,8 +33,8 @@
             WhatsApp @ +65 8505 5484. For updates on your case, you may access it with your registered email
             as follows;<br><br>
 
-            Login Page: <a href="https://www.securre.net/dcmslogin">DCMS LOGIN</a> <br>
-            Login ID: {{$client->email}} <br>
+            Login Page: <a href="{{ env('APP_URL') }}">DCMS LOGIN</a> <br>
+            Login ID: {{ $client->email }} <br>
             Password: 12345678 <br><br>
 
             It is an honour to be working on your case/s, and we appreciate your faith in our good service.
@@ -40,8 +43,9 @@
 
             In the meantime, please feel free to check out our website for more information and here's a
             small introduction on how we were listed on Lianhe Wanbao and Straits Times for our good work!
-            <a href="http://dcms.test/">DCMS</a>
+            <a href="{{ env('APP_URL') }}">DCMS</a>
         </div>
     </div>
 </body>
+
 </html>
